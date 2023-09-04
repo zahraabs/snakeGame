5248d6aaebc9923a8cc0242fa11b8307cf880e0e
zahraabs <zahraabbasiun@gmail.com>
2023-09-04 23:45:00 +0330

    reset game and score of game functions added

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 const canvas = document.querySelector("canvas");
+const reset = document.querySelector("button");
 const context = canvas.getContext("2d");
+
 let seedX;
 let seedY;
-let dirX =10;
+let dirX = 10;
 let dirY = 0;
+let score = 0;
 
 let flag = false;
 
 canvas.width = 400;
 canvas.height = 400;
 
-document.addEventListener("keydown" , changeDir);
+document.addEventListener("keydown", changeDir);
+reset.addEventListener("click", resetGame);
 
 context.fillStyle = "white";
 context.strokeStyle = "black";
@@ -24,7 +28,7 @@ let snake = [
     { x: 110, y: 100 },
 ]
 
-let snakeDraw = ()=>{
+let snakeDraw = () => {
     snake.forEach(item => {
         context.fillStyle = "aqua";
         context.strokeStyle = "red";
@@ -55,21 +59,27 @@ let seedDraw = () => {
     context.strokeRect(seedX, seedY, 10, 10);
 }
 
-function moving () {
+function moving() {
     const newPosition = {
-        x : snake[0].x + dirX ,
-        y : snake[0].y + dirY
+        x: snake[0].x + dirX,
+        y: snake[0].y + dirY
     }
     snake.unshift(newPosition);
     if (newPosition.x == seedX && newPosition.y == seedY) {
+        score++;
+        showScore();
         generateSeed()
-    }else{
+    } else {
         snake.pop();
     }
-    
+
 }
 
-function clear () {
+function showScore() {
+    document.querySelector(".setting > h1").innerHTML = score;
+}
+
+function clear() {
     context.fillStyle = "white";
     context.strokeStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height)
@@ -78,16 +88,16 @@ function clear () {
 
 function changeDir(e) {
     let pressed = e.keyCode;
-    if (flag) return  ;
-    flag = true;    
-    
+    if (flag) return;
+    flag = true;
+
     if (pressed == 37 && dirX != 10) {
         dirX = -10;
         dirY = 0;
-    }   if (pressed == 39 && dirX != -10) {
+    } if (pressed == 39 && dirX != -10) {
         dirX = 10;
         dirY = 0;
-    }  
+    }
     if (pressed == 38 && dirY != 10) {
         dirX = 0;
         dirY = -10;
@@ -100,12 +110,31 @@ function changeDir(e) {
 function gameOver() {
     for (let i = 1; i < snake.length; i++) {
         // console.log(snake[0].x , snake[i].x ,snake[0].y , snake[i].y);
-     if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) return true;  
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) return true;
     }
-    if (snake[0].x == 0) return true;
-    if (snake[0].x >= canvas.width -10) return true;
-    if (snake[0].y == 0) return true;
-    if (snake[0].y >= canvas.height -10) return true;
+    if (snake[0].x < 0) return true;
+    if (snake[0].x >= canvas.width) return true;
+    if (snake[0].y < 0) return true;
+    if (snake[0].y >= canvas.height) return true;
+}
+
+function resetGame() {
+
+    snake = [
+        { x: 140, y: 100 },
+        { x: 130, y: 100 },
+        { x: 120, y: 100 },
+        { x: 110, y: 100 },
+    ];
+
+    score = 0;
+    seedX; seedY;
+    dirX = 10; dirY = 0;
+
+    flag = false;
+    showScore();
+    generateSeed();
+    run();
 }
 
 generateSeed();
@@ -113,15 +142,15 @@ run();
 function run() {
 
     if (gameOver()) return;
-    
-    setTimeout(()=> {
-        flag=false;
+
+    setTimeout(() => {
+        flag = false;
         clear();
         seedDraw();
         moving();
         snakeDraw();
 
         run();
-    } , 100) 
+    }, 100)
 }
 
